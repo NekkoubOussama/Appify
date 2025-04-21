46c90a702f737b5703cca0e6202a69f796308d50
NekkoubOussama <nekkoub.oussama@gmail.com>
2025-04-21 22:47:51 +0100

    hero

--- components/Globalhome/hero.tsx
+++ components/Globalhome/hero.tsx
@@ -12,32 +12,30 @@ const appLinks = [
 
 export default function Hero() {
   return (
-    <section className=" bg-bg1 h-screen pt-[3vh] md:pt-[12vh] w-full ">
+    <section className=" bg-bg1 h-screen pt-[8vh] md:pt-[12vh] w-full pb-[2vh] ">
       <div className="flex justify-center items-center w-[90%] sm:w-[80%] h-full mx-auto">
         <div className="lg:w-[55%] ">
-          <div className="w-fit bg-white rounded-4xl space-x-3 flex items-center justify-center py-1.5 px-5 shadow-md animate-fade-left">
-            <div className="bg-primary text-white py-[0.25rem] px-[1.25rem] rounded-full">
+          <div className="w-fit bg-white rounded-4xl space-x-3 flex items-center justify-center py-1.5 px-5 shadow-md fade-left text-[0.75rem] lg:text-[0.875rem] lg:leading-[1.25rem]">
+            <div className="bg-primary text-white lg:py-[0.25rem] lg:px-[1.25rem] px-3 py-1 rounded-full text-[1rem] leading-[1.5rem]">
               News
             </div>
-            <p className="text-[0.875rem] leading-[1.25rem]">
-              We have updated our term & condition policy
-            </p>
+            <p>We have updated our term & condition policy</p>
           </div>
-          <h1 className="my-6 animate-fade-in ">
+          <h1 className="my-6 fade-in ">
             The premier workspace companion for your daily needs.
           </h1>
-          <p className="text-grey animate-fade-in ">
+          <p className="text-grey fade-in ">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
             ipsam laudantium qui velit assumenda commodi laboriosam cumque
             laborum perferendis totam.
           </p>
-          <div className="flex space-x-4 my-8 animate-pop">
+          <div className="flex space-x-4 my-8 pop">
             {appLinks.map((appLink, index) => (
               <LinkApp link={appLink.href} icon={appLink.icon} key={index} />
             ))}
           </div>
         </div>
-        <div className="hidden lg:block animate-fade-in delay-500">
+        <div className="hidden lg:block fade-in delay-500">
           <Image src={Phero} alt="Hero picture" width={700} height={700} />
         </div>
       </div>
