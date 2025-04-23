f38f5c15ae956d7b4a259c58483d1d25714fba56
NekkoubOussama <nekkoub.oussama@gmail.com>
2025-04-23 21:57:03 +0100

    dinamique logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import clsx from "clsx";
 
-export default function Logo() {
+export default function Logo({ color = "text-black" }) {
   return (
-    <p className="text-xl md:text-2xl font-bold leading-8 cursor-context-menu">
+    <p
+      className={clsx(
+        "text-xl md:text-2xl font-bold leading-8 cursor-context-menu",
+        color
+      )}
+    >
       <span className="text-3xl md:text-4xl text-pink-700 leading-8">A</span>
       ppify
     </p>
